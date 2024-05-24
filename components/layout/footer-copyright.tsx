46dc17d0258d0fca955b1029313bd4dcb5c0fa21
mohansky <38560439+mohansky@users.noreply.github.com>
2024-05-24 16:05:45 +0530

    Update footer-copyright.tsx

--- components/layout/footer-copyright.tsx
+++ components/layout/footer-copyright.tsx
@@ -1,10 +1,10 @@
-import FooterContactData from "@/data/footercontact.json"; 
+import { options } from "@/.velite";
 
 export default function Copyright() {
   return (
     <>
-      <div className="text-center text-xs font-thin pb-2">
-          <p>{FooterContactData.copyright}</p>
+      <div className="text-accent text-center text-xs font-thin pb-2">
+          <p>{options.copyright}</p>
       </div>
     </>
   );
