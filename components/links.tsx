6e6ea7adf0c782882c9ee5124cc5f874e7acdafa
mohansky <38560439+mohansky@users.noreply.github.com>
2024-05-24 16:27:16 +0530

    Delete components directory

--- components/links.tsx
+++
@@ -1,7 +0,0 @@
-import Link from "next/link";
-
-export default function Links({href, className, children}: any) {
-  return (
-    <Link href={href} className={`text-gray-900 hover:text-gray-600 ${className}`}>{children}</Link>
-  )
-}
