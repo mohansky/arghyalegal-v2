b788569ffadbec6d76f055541114919fe1ac1773
mohansky <38560439+mohansky@users.noreply.github.com>
2024-05-30 10:42:07 +0530

    Update whyus-home.tsx

--- components/whyus-home.tsx
+++ components/whyus-home.tsx
@@ -5,10 +5,7 @@ import { options } from "@/.velite";
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import { Heading } from "./styled-comps/heading";
 
@@ -18,7 +15,7 @@ export default function WhyusHome() {
       <Card className="grid md:grid-cols-5 md:gap-8 border-0 shadow-none">
         <div className="col-span-5 md:col-span-2 w-full rounded-md mb-5">
           <Image
-            src="/images/why-us-home.jpg"
+            src={options.whyusimage!}
             alt="Why us"
             title="Why us"
             width={1920}
@@ -29,17 +26,11 @@ export default function WhyusHome() {
         <div className="col-span-3">
           <CardHeader className="pt-0">
             <Heading size="md" className="font-semibold">
-              Why Us
+              {options.whyustitle}
             </Heading>
           </CardHeader>
           <CardContent>
-            {/* <p className="mb-5">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using &apos;Content here,
-              content here&apos;, making it look like readable English.
-            </p> */}
+            <p className="mb-5">{options.whyussubtitle} </p>
             <ul className="space-y-4">
               {options.whyusitems.map((whyus) => (
                 <li key={whyus.id} className="flex items-center font-medium">
