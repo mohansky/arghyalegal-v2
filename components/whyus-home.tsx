7ca29bca55a4541784c9ecbaebaa222cd90853cb
mohansky <38560439+mohansky@users.noreply.github.com>
2024-05-30 18:03:19 +0530

    Update whyus-home.tsx

--- components/whyus-home.tsx
+++ components/whyus-home.tsx
@@ -2,17 +2,16 @@
 import Image from "next/image";
 import { Icon } from "@iconify/react";
 import { options } from "@/.velite";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Heading } from "./styled-comps/heading";
 import { Paragraph } from "./styled-comps/paragraph";
 
 export default function WhyusHome() {
   return (
     <>
+      <Heading size="md" className="font-semibold mb-5">
+        {options.whyustitle}
+      </Heading>
       <Card className="grid md:grid-cols-5 md:gap-8 border-0 shadow-none">
         <div className="col-span-5 md:col-span-2 w-full rounded-md mb-5">
           <Image
@@ -21,18 +20,13 @@ export default function WhyusHome() {
             title="Why us"
             width={1920}
             height={1080}
-            className="mx-auto text-center"
+            className="object-cover h-full w-full mx-auto text-center"
           />
         </div>
         <div className="col-span-3">
-          <CardHeader className="pt-0">
-            <Heading size="md" className="font-semibold">
-              {options.whyustitle}
-            </Heading>
-          </CardHeader>
           <CardContent>
             <Paragraph className="mb-5">{options.whyussubtitle} </Paragraph>
-            <ul className="space-y-4">
+            <ul className="space-y-3">
               {options.whyusitems.map((whyus) => (
                 <li key={whyus.id} className="flex items-center font-medium">
                   <Icon
