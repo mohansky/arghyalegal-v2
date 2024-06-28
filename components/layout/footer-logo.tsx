b5d0dc0940a179ca0c64c42ec54aa5b32f10668c
mohansky <38560439+mohansky@users.noreply.github.com>
2024-06-28 19:00:46 +0530

    Update footer-logo.tsx

--- components/layout/footer-logo.tsx
+++ components/layout/footer-logo.tsx
@@ -8,7 +8,7 @@ export default function FooterLogo() {
       <Links href="/" title="back to homepage" className="mb-5">
       <Image
           className="mx-auto mb-3"
-          src="/images/logos/al-logo.png"
+            src="/images/logos/al-logo-v2.svg"
           alt="Argya Legal"
           width="100"
           height="100"
